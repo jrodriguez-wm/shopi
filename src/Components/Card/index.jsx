8f1a9ad67e34ed0ef1275c7a89771f428f0c362d
jrodriguez-wm <jorge.rodriguez@webmaps.com.mx>
2024-02-23 18:54:43 -0600

    Consumo de API con useEffect y generación de catalogo de productos en home, reemplazando info del API en nuestras Cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,18 +1,21 @@
-const Card = ()=>{
+const Card = (data)=>{
     return(
+        
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'> 
+            {/* <p> {JSON.stringify(data.data)} </p> */}
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
-                    Electronics
+                    {data.data.category}
+                    
                 </span>
-                <img className='w-full h-full object-cover rounded-lg' src='https://media.istockphoto.com/id/1171805415/es/foto/auriculares-amarillos-sobre-fondo-azul-concepto-musical.jpg?s=612x612&w=0&k=20&c=5Cbm_7gJsIL1ZNGwVlzoxfjROa0W3SqlOrH7GtR5kkE=' alt='headphones' />
+                <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.category} />
                 <div className="absolute top-0 right-0 flex justify-center items-center rounded-full w-6 h-6 bg-white m-2 p-1 font-bold">
                     +
                 </div>
             </figure>
             <p className="flex justify-between">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-bold">$300</span>
+                <span className="text-sm font-light">{data.data.title}</span>
+                <span className="text-lg font-bold">${data.data.price}</span>
             </p>
         </div>
     );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from 'react';
+
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
+
 function Home() {
+
+  const [items,setItems] = useState(null);
+  
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  },[])
+
+
   return (
     <Layout>
         Home
-        <Card/>
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map(item=>(
+              //  <p> {JSON.stringify(item)} </p> 
+              <Card key={item.id} data={item}/>
+            ))
+          }
+        </div>
+        
+        
     </Layout>    
   )
 }
